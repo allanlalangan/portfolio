6cc14915d6c2142998a8a1b2e8959907edc36869
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-13 00:55:06 -0700

    refactor: create svg icon components and replace img tags in Skills component

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -1,5 +1,15 @@
 import { FaGithub } from 'react-icons/fa';
-import Image from 'next/image';
+import {
+  HtmlIcon,
+  CssIcon,
+  JsIcon,
+  ReactIcon,
+  ReduxIcon,
+  SassIcon,
+  MuiIcon,
+  NpmIcon,
+} from '../ui/icons';
+
 import styles from './Skills.module.scss';
 
 const Skills = () => {
@@ -8,84 +18,47 @@ const Skills = () => {
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
-        <img
-          alt='html5 icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
-        />
+        <HtmlIcon className={styles['svg-icon']} />
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
-        <img
-          alt='css3 icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
-        />
+        <CssIcon className={styles['svg-icon']} />
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
-        <img
-          alt='javascript icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
-        />
+        <JsIcon className={styles['svg-icon']} />
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
-        <img
-          alt='reactjs icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
-        />
+        <ReactIcon className={styles['svg-icon']} />
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
-        <img
-          alt='redux icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
-        />
+        <ReduxIcon className={styles['svg-icon']} />
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
-        <img
-          alt='sass icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg'
-        />
+        <SassIcon className={styles['svg-icon']} />
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
-        <img
-          alt='materialui icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg'
-        />
+        <MuiIcon className={styles['svg-icon']} />
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
         <FaGithub aria-label='github icon' className={styles['skills__icon']} />
       </figure>
-      {/* <img
-        alt='icon'
-        className={styles['svg-icon']}
-        src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'
-      /> */}
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
-        <img
-          alt='npm icon'
-          className={styles['svg-icon']}
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg'
-        />
+        <NpmIcon className={styles['svg-icon']} />
       </figure>
     </article>
   );
---
+++ components/ui/icons/index.js
@@ -0,0 +1,9 @@
+export { default as HtmlIcon } from './HtmlIcon';
+export { default as CssIcon } from './CssIcon';
+export { default as JsIcon } from './JsIcon';
+export { default as ReactIcon } from './ReactIcon';
+export { default as ReduxIcon } from './ReduxIcon';
+export { default as SassIcon } from './SassIcon';
+export { default as MuiIcon } from './MuiIcon';
+export { default as NpmIcon } from './NpmIcon';
+export { default as GithubIcon } from './GithubIcon';
