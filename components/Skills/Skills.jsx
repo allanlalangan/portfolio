c132a452d2ab41f4a5d9ca64a78c02465990d7b6
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-20 02:08:55 -0700

    feat: update Skills with icon figcaptions

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -19,46 +19,55 @@ const Skills = () => {
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
         <HtmlIcon className={styles['svg-icon']} />
+        <figcaption>html</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
         <CssIcon className={styles['svg-icon']} />
+        <figcaption>css</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
         <JsIcon className={styles['svg-icon']} />
+        <figcaption>javascript</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
         <ReactIcon className={styles['svg-icon']} />
+        <figcaption>react</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
       >
         <ReduxIcon className={styles['svg-icon']} />
+        <figcaption>redux</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
         <SassIcon className={styles['svg-icon']} />
+        <figcaption>sass</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
         <MuiIcon className={styles['svg-icon']} />
+        <figcaption>material ui</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
         <FaGithub aria-label='github icon' className={styles['skills__icon']} />
+        <figcaption>github</figcaption>
       </figure>
       <figure
         className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
       >
         <NpmIcon className={styles['svg-icon']} />
+        <figcaption>npm</figcaption>
       </figure>
     </article>
   );
