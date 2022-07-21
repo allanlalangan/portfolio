dcb1ce40242b42baa01c8043f7d9ba65d4f25a3c
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-20 21:40:41 -0700

    feat(Skills): update responsive layout

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -16,55 +16,55 @@ const Skills = () => {
   return (
     <article className={`${styles.skills} grid-row`}>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-lg`}
       >
         <HtmlIcon className={styles['svg-icon']} />
         <figcaption>html</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-lg`}
       >
         <CssIcon className={styles['svg-icon']} />
         <figcaption>css</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-lg`}
       >
         <JsIcon className={styles['svg-icon']} />
         <figcaption>javascript</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-lg`}
       >
         <ReactIcon className={styles['svg-icon']} />
         <figcaption>react</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-2-sm col-1-lg`}
       >
         <ReduxIcon className={styles['svg-icon']} />
         <figcaption>redux</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-lg`}
       >
         <SassIcon className={styles['svg-icon']} />
         <figcaption>sass</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-lg`}
       >
         <MuiIcon className={styles['svg-icon']} />
         <figcaption>material ui</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-lg`}
       >
         <FaGithub aria-label='github icon' className={styles['skills__icon']} />
         <figcaption>github</figcaption>
       </figure>
       <figure
-        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-md`}
+        className={`${styles['icon-container']} col-4-xs col-3-sm col-1-lg`}
       >
         <NpmIcon className={styles['svg-icon']} />
         <figcaption>npm</figcaption>
