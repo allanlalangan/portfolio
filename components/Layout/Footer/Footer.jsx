ed69d337e29495fdd441ffb07c01a60544660d4c
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-30 10:19:22 -0700

    feat: init Footer, add to Layout

---
+++ components/Layout/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      <h2 className={styles.heading}>
+        Designed and built by Allan Lalangan © 2022
+      </h2>
+    </footer>
+  );
+};
+export default Footer;
