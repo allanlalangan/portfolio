8e1be3a76a6c3d3b76c720601d1e984585650b4b
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-11 08:24:43 -0700

    feat(About): modify pattern position relative to Skills section
    update bio

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -20,61 +20,24 @@ const About = () => {
   const { theme } = useContext(ThemeContext);
   return (
     <>
-      <div className={`${styles.pattern} ${styles[theme]}`}></div>
+      {/* <div className={`${styles.pattern} ${styles[theme]}`}></div> */}
       <section className={styles.container}>
-        <h2 className={`${styles.greeting} ${styles.greeting}`}>Hello 👋</h2>
+        <h2 className={`${styles.greeting} ${styles.greeting}`}>Hello! 👋</h2>
         <article className={styles.bio}>
           <p className={styles.__text}>
-            I&apos;m Allan; a Portland, OR based web developer, life long
-            learner, and perpetual dreamer. I build responsive web apps that are
-            easy to use, and look good. All styled, themed and designed by me.
+            I&apos;m Allan. Web developer, life long learner, and daydreamer. I
+            build responsive web apps that look good and are easy to use. All
+            designed, themed and styled by me.
+          </p>
+          <p className={styles.__text}>
+            I&apos;ve been coding for 5 years and currently transitioning
+            careers from the service industry. My present focus is to gain
+            experience and improve my skills while learning industry standards
+            and conventions.
           </p>
         </article>
 
-        <h2>Skills</h2>
-        <article className={styles.skills}>
-          <h3>My main tools consist of:</h3>
-          <ul>
-            <li>ReactJS, hooks and React Router</li>
-            <li>Javascript / ES6</li>
-            <li>Semantic html</li>
-            <li>
-              Styling and design with modern CSS, Sass and frameworks such as
-              Material UI and Tailwind CSS
-            </li>
-            <li>State management with Redux + RTK</li>
-            <li>Familiar with MERN stack, JSON Web Tokens (JWT)</li>
-          </ul>
-          {/* <h3>Fundamentals</h3>
-        <figure>
-          <HtmlIcon className={styles['svg-icon']} />
-          <CssIcon className={styles['svg-icon']} />
-          <JsIcon className={styles['svg-icon']} />
-          <figcaption>Semantic html, CSS modules, and Javascript</figcaption>
-        </figure>
-        <h3>Front end libraries and state management</h3>
-        <figure>
-          <ReactIcon className={styles['svg-icon']} />
-          <ReduxIcon className={styles['svg-icon']} />
-          <SiNextdotjs size={'3rem'} />
-        </figure>
-        <h3>Tools</h3>
-        <figure>
-          <FaGithub className={styles['font-icon']} />
-          <NpmIcon className={styles['svg-icon']} />
-        </figure>
-        <h3>UI</h3>
-        <figure>
-          <SassIcon className={styles['svg-icon']} />
-          <MuiIcon className={styles['svg-icon']} />
-          <figcaption>
-            Custom design, templates, themes and styled components with Sass and
-            Material UI
-          </figcaption>
-        </figure> */}
-        </article>
-
-        <h2 className={`${styles.heading}`}>What I am currently learning:</h2>
+        {/* <h2 className={`${styles.heading}`}>What I am currently learning:</h2>
 
         <article className={`${styles['currently-learning']}`}>
           <h3 className={styles.__subheading}>ARIA and accessibility</h3>
@@ -94,7 +57,7 @@ const About = () => {
             SASS has made styling an easier and more streamlined process with
             mixins, variables etc.
           </p>
-        </article>
+        </article> */}
       </section>
     </>
   );
