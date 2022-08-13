f91e4e7be21d341be3dbfa1778c92348a653bf3e
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-12 23:39:02 -0700

    feat: update bio

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -31,9 +31,16 @@ const About = () => {
           </p>
           <p className={styles.__text}>
             I&apos;ve been coding for 5 years and currently transitioning
-            careers from the service industry. My present focus is to gain
-            experience and improve my skills while learning industry standards
+            careers from the service industry. My focus is to gain experience
+            and continue to improve my skills while learning industry standards
             and conventions.
+          </p>
+          <p className={styles.__text}>
+            I build single and multi page applications with client and server
+            side rendering. I can build static or dynamic websites that
+            implement a backend and interact with users. I am here to help you
+            grow your vision and improve your product through better user
+            experience.
           </p>
         </article>
 
