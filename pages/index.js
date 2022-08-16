683c71fd093cc32919793f9da6601821ecddd5b7
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-15 21:21:41 -0700

    chore(index): delete comments, unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,9 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from './index.module.scss';
 import { projects, otherProjects, about } from '../public/data';
 
 // components
-import {
-  Hero,
-  SkillsBanner,
-  Divider,
-  ProjectCard,
-  About,
-  Contact,
-} from '../components';
-import {
-  CssIcon,
-  HtmlIcon,
-  JsIcon,
-  MuiIcon,
-  NpmIcon,
-  ReactIcon,
-  ReduxIcon,
-  SassIcon,
-} from '../components/ui/icons';
-import { FaGithub } from 'react-icons/fa';
-import { SiNextdotjs } from 'react-icons/si';
+import { Hero, SkillsBanner, ProjectCard, About, Contact } from '../components';
 import { useContext } from 'react';
 import { ThemeContext } from '../store/ThemeContextProvider';
 import Skills from '../components/Skills/Skills';
@@ -39,27 +19,23 @@ export default function Home() {
       </Head>
       <>
         <Hero />
-        {/* <Divider /> */}
+
         <h1
           className={`${styles[theme]} ${styles.heading} ${styles['contrast-heading']}`}
         >
           Recent Projects
         </h1>
         <section className={styles.projects}>
-          <ul>
-            {projects.map((project) => (
-              <ProjectCard key={project.title} project={project} />
-            ))}
-          </ul>
-          <ul>
-            {otherProjects.map((project) => (
-              <OtherProject key={project.title} project={project} />
-            ))}
-          </ul>
+          {projects.map((project) => (
+            <ProjectCard key={project.title} project={project} />
+          ))}
+
+          {otherProjects.map((project) => (
+            <OtherProject key={project.title} project={project} />
+          ))}
         </section>
         <section className={styles['other-projects']}></section>
-        {/* <Divider /> */}
-        {/* <h1 className={styles.heading}>Primary Skills</h1> */}
+
         <SkillsBanner />
 
         <h1 className={`${styles[theme]} ${styles.heading}`}>Skills</h1>
@@ -69,7 +45,7 @@ export default function Home() {
         <section className={styles.about}>
           <About />
         </section>
-        {/* <Divider /> */}
+
         <h1
           className={`${styles[theme]} ${styles.heading} ${styles['contrast-heading']}`}
         >
@@ -78,9 +54,6 @@ export default function Home() {
         <section className={styles.contact}>
           <Contact />
         </section>
-        {/* <button className={styles['action-button']}>
-          {about.actionCall_2}
-        </button> */}
       </>
     </>
   );
