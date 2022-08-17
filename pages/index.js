3675dfa68e298d893045c8f18659955a071e1c86
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-17 08:51:58 -0700

    add favicon, fix li bullet aligment

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,10 @@ const Home = () => {
     <>
       <Head>
         <title>Allan Lalangan | Web Developer</title>
+        <link
+          rel='icon'
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👹</text></svg>"
+        />
         <meta
           name='description'
           content='A personal portfolio website for Allan Lalangan'
