a0293c048ecab1c49b048ff11b5eed55ad3c02a3
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-17 01:55:37 -0700

    feat(Skills): update bullet and bold text styles

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../../store/ThemeContextProvider';
 import styles from './Skills.module.scss';
+import { CgShapeRhombus } from 'react-icons/cg';
 
 const Skills = () => {
   const { theme } = useContext(ThemeContext);
@@ -9,16 +10,71 @@ const Skills = () => {
       <section className={styles.skills}>
         <h3 className={styles.heading}>My primary tools consist of:</h3>
         <ul className={styles.list}>
-          <li className={styles.skill}>ReactJS, hooks and React Router</li>
-          <li className={styles.skill}>Javascript / ES6</li>
-          <li className={styles.skill}>Semantic html</li>
           <li className={styles.skill}>
-            Styling and design with modern CSS, Sass and frameworks such as
-            Tailwind CSS and Material UI
+            <span className={styles['bullet-container']}>
+              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+            </span>
+            <p className={styles.caption}>
+              <strong className={`${styles.strong} ${styles[theme]}`}>
+                ReactJS
+              </strong>
+              , hooks and React Router
+            </p>
           </li>
-          <li className={styles.skill}>State management with Redux + RTK</li>
           <li className={styles.skill}>
-            Familiar with MERN stack, JSON Web Tokens (JWT)
+            <span className={styles['bullet-container']}>
+              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+            </span>
+            <p className={styles.caption}>Javascript / ES6</p>
+          </li>
+          <li className={styles.skill}>
+            <span className={styles['bullet-container']}>
+              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+            </span>
+            <p className={styles.caption}>
+              <strong className={`${styles.strong} ${styles[theme]}`}>
+                Semantic
+              </strong>{' '}
+              html
+            </p>
+          </li>
+          <li className={styles.skill}>
+            <span className={styles['bullet-container']}>
+              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+            </span>
+            <p className={styles.caption}>
+              Styling and design with modern{' '}
+              <strong className={`${styles.strong} ${styles[theme]}`}>
+                CSS
+              </strong>
+              ,{' '}
+              <strong className={`${styles.strong} ${styles[theme]}`}>
+                Sass
+              </strong>{' '}
+              and frameworks such as strong and{' '}
+              <strong className={`${styles.strong} ${styles[theme]}`}>
+                Material UI
+              </strong>
+            </p>
+          </li>
+          <li className={styles.skill}>
+            <span className={styles['bullet-container']}>
+              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+            </span>
+            <p className={styles.caption}>
+              State management with{' '}
+              <strong className={`${styles.strong} ${styles[theme]}`}>
+                Redux + RTK
+              </strong>
+            </p>
+          </li>
+          <li className={styles.skill}>
+            <span className={styles['bullet-container']}>
+              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+            </span>
+            <p className={styles.caption}>
+              Familiar with MERN stack, JSON Web Tokens (JWT)
+            </p>
           </li>
         </ul>
       </section>
