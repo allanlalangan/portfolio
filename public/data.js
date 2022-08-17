6b6bbbfca0fe656a12067037cd4ee6038ab8b248
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-17 00:05:50 -0700

    modify projects data

--- public/data.js
+++ public/data.js
@@ -5,10 +5,7 @@ export const about = {
   greeting: 'Hello World. I am',
   brief:
     'I build responsive and beautifully designed websites that are easy to use.',
-  brief_2: 'I build things!',
   actionCall: 'View my work',
-  actionCall_2: 'Get in touch',
-  actionCall_3: 'I can show you better than I can tell you...',
   cv: '/',
 };
 
@@ -20,144 +17,6 @@ export const contact = {
     { name: 'github', icon: '', url: 'https://github.com/allanlalangan' },
   ],
 };
-
-export const skills = [
-  {
-    name: 'html',
-    icon: '',
-    category: 'language',
-    caption: 'base',
-    favorite: false,
-  },
-  {
-    name: 'css',
-    icon: '',
-    category: 'language',
-    caption: 'base',
-    favorite: false,
-  },
-  {
-    name: 'javascript',
-    icon: '',
-    category: 'language',
-    caption: 'base',
-    favorite: false,
-  },
-  {
-    name: 'node.js',
-    icon: '',
-    category: 'language',
-    caption: 'beginner',
-    favorite: false,
-  },
-  {
-    name: 'express',
-    icon: '',
-    category: 'framework',
-    caption: 'beginner',
-    favorite: false,
-  },
-  {
-    name: 'node.js',
-    icon: '',
-    category: 'language',
-    caption: 'beginner',
-    favorite: false,
-  },
-  {
-    name: 'mongoDb',
-    icon: '',
-    category: 'database',
-    caption: 'beginner',
-    favorite: false,
-  },
-  {
-    name: 'mongoose',
-    icon: '',
-    category: 'framework',
-    caption: 'beginner',
-    favorite: false,
-  },
-  {
-    name: 'react',
-    icon: '',
-    category: 'framework',
-    caption: '',
-    favorite: true,
-  },
-  { name: 'redux', icon: '', category: 'tool', caption: '', favorite: false },
-  {
-    name: 'next.js',
-    icon: '',
-    category: 'framework',
-    caption: '',
-    favorite: true,
-  },
-  {
-    name: 'materialui',
-    icon: '',
-    category: 'library',
-    caption: '',
-    favorite: false,
-  },
-  {
-    name: 'framer motion',
-    icon: '',
-    category: 'library',
-    caption: 'animation',
-    favorite: false,
-  },
-  {
-    name: 'github',
-    icon: '',
-    category: 'version control',
-    caption: '',
-    favorite: false,
-  },
-  {
-    name: 'git',
-    icon: '',
-    category: 'version control',
-    caption: '',
-    favorite: false,
-  },
-  {
-    name: 'npm',
-    icon: '',
-    category: 'package manager',
-    caption: '',
-    favorite: false,
-  },
-  {
-    name: 'axios',
-    icon: '',
-    category: 'tool',
-    caption: 'http requests',
-    favorite: false,
-  },
-  { name: 'rest', icon: '', category: 'tool', caption: '', favorite: false },
-  {
-    name: 'firebase',
-    icon: '',
-    category: 'service',
-    caption: 'beginner',
-    favorite: false,
-  },
-  {
-    name: 'stripe',
-    icon: '',
-    category: 'payments',
-    caption: 'beginner',
-    favorite: false,
-  },
-  {
-    name: 'json web tokens',
-    icon: '',
-    category: 'authentication',
-    caption: 'beginner',
-    favorite: false,
-  },
-];
 
 import nomad from '../assets/nomad-travel.png';
 import dashboard from '../assets/dashboard.png';
@@ -174,24 +33,24 @@ export const projects = [
     url: 'https://nononomad.netlify.app/',
     complete: false,
   },
-  {
-    title: 'project.Me',
-    description:
-      'A fullstack to-do list app. Registered users can save goals. Register new user or Login with username: demo@demo.com password: demo',
-    tech: [
-      'express',
-      'mongodb',
-      'react',
-      'react router',
-      'sass',
-      'redux',
-      'web tokens',
-    ],
-    image: { src: dashboard, alt: '' },
-    repo: 'https://github.com/allanlalangan/dashboard',
-    url: '/',
-    complete: false,
-  },
+  // {
+  //   title: 'project.Me',
+  //   description:
+  //     'A fullstack to-do list app. Registered users can save goals. Register new user or Login with username: demo@demo.com password: demo',
+  //   tech: [
+  //     'express',
+  //     'mongodb',
+  //     'react',
+  //     'react router',
+  //     'sass',
+  //     'redux',
+  //     'web tokens',
+  //   ],
+  //   image: { src: dashboard, alt: '' },
+  //   repo: 'https://github.com/allanlalangan/dashboard',
+  //   url: '',
+  //   complete: false,
+  // },
   {
     title: 'Martial Masters Academy',
     description:
@@ -199,7 +58,7 @@ export const projects = [
     tech: ['next', 'tailwind css'],
     image: { src: mma, alt: '' },
     repo: 'https://github.com/allanlalangan/martial-masters-academy',
-    url: '/',
+    url: 'https://mma-acdmy.netlify.app/',
     complete: true,
   },
 ];
