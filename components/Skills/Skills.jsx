386a2dcbd67848e95b88c97cbab5d15b682e8adb
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-12-20 10:55:55 -0800

    fix: change to proper heading order

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -6,137 +6,133 @@ import { CgShapeRhombus } from 'react-icons/cg';
 const Skills = () => {
   const { theme } = useContext(ThemeContext);
   return (
-    <>
-      <section className={styles.skills}>
-        <h3 className={styles.heading}>My primary tools include:</h3>
-        <ul className={styles.list}>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Semantic
-              </strong>{' '}
-              HTML
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Javascript / ES6
-              </strong>
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              Styling and design with{' '}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                CSS
-              </strong>
-              ,{' '}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Sass
-              </strong>{' '}
-              and frameworks such as{' '}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Tailwind CSS
-              </strong>{' '}
-              and{' '}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Material UI
-              </strong>
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                ReactJS
-              </strong>
-              , hooks and React{' '}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Router
-              </strong>
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              {`State management with `}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Redux + Toolkit
-              </strong>
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              {`Source control with `}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Github
-              </strong>
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              Building {` `}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                RESTful APIs
-              </strong>
-              {` `}with{` `}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                MongoDB
-              </strong>
-              {', '}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Express
-              </strong>
-              {', and '}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Node.js
-              </strong>
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              API testing with{` `}
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                Postman
-              </strong>
-            </p>
-          </li>
-          <li className={styles.skill}>
-            <span className={styles['bullet-container']}>
-              <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
-            </span>
-            <p className={styles.caption}>
-              <strong className={`${styles.strong} ${styles[theme]}`}>
-                User authentication
-              </strong>
-            </p>
-          </li>
-        </ul>
-      </section>
-    </>
+    <section className={styles.skills}>
+      <h2 className={styles.heading}>My primary tools include:</h2>
+      <ul className={styles.list}>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Semantic
+            </strong>{' '}
+            HTML
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Javascript / ES6
+            </strong>
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            Styling and design with{' '}
+            <strong className={`${styles.strong} ${styles[theme]}`}>CSS</strong>
+            ,{' '}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Sass
+            </strong>{' '}
+            and frameworks such as{' '}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Tailwind CSS
+            </strong>{' '}
+            and{' '}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Material UI
+            </strong>
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              ReactJS
+            </strong>
+            , hooks and React{' '}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Router
+            </strong>
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            {`State management with `}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Redux + Toolkit
+            </strong>
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            {`Source control with `}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Github
+            </strong>
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            Building {` `}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              RESTful APIs
+            </strong>
+            {` `}with{` `}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              MongoDB
+            </strong>
+            {', '}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Express
+            </strong>
+            {', and '}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Node.js
+            </strong>
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            API testing with{` `}
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              Postman
+            </strong>
+          </p>
+        </li>
+        <li className={styles.skill}>
+          <span className={styles['bullet-container']}>
+            <CgShapeRhombus className={`${styles.bullet} ${styles[theme]}`} />
+          </span>
+          <p className={styles.caption}>
+            <strong className={`${styles.strong} ${styles[theme]}`}>
+              User authentication
+            </strong>
+          </p>
+        </li>
+      </ul>
+    </section>
   );
 };
 export default Skills;
