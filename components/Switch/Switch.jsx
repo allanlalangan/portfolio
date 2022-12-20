08c4536db682435a9526b177cbab234947260ec7
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-12-20 13:07:15 -0800

    feat: add accessibility to dark theme toggle

--- components/Switch/Switch.jsx
+++ components/Switch/Switch.jsx
@@ -6,20 +6,25 @@ import styles from './Switch.module.scss';
 const Switch = () => {
   const { theme, switchTheme } = useContext(ThemeContext);
 
-  useEffect(() => {
-    console.log(theme);
-  }, [theme]);
-
   return (
-    <aside className={styles['container']}>
-      <button className={styles.switch} onClick={switchTheme}>
-        {theme === 'light' ? (
-          <FaSun className={`${styles['icon']} ${styles['sun']}`} />
-        ) : (
-          <FaMoon className={`${styles['icon']} ${styles['moon']}`} />
-        )}
-      </button>
-    </aside>
+    <label aria-hidden={true} htmlFor='darkMode' className={styles['label']}>
+      {theme === 'light' ? (
+        <FaSun className={`${styles['icon']} ${styles['sun']}`} />
+      ) : (
+        <FaMoon className={`${styles['icon']} ${styles['moon']}`} />
+      )}
+      <input
+        id='darkMode'
+        name='darkMode'
+        type='checkbox'
+        role='switch'
+        className={styles.checkbox}
+        onChange={switchTheme}
+        checked={theme === 'dark' ? true : false}
+        aria-checked={theme === 'dark' ? true : false}
+      />
+      <span className={styles['label-text']}>Dark Theme Toggle</span>
+    </label>
   );
 };
 export default Switch;
