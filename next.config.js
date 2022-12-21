aa9045fd30a0227bb7ed88f5cc15df45a4d4f5aa
allanlalangan <allanlalangan@gmail.com>
2022-12-20 20:00:51 -0800

    build: update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  locales: ["en"],
+  defaultLocale: "en",
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
