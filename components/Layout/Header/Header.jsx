4c17f348a0528e4ba93d8989fcfaba73936d4216
allanlalangan <allanlalangan@gmail.com>
2022-12-28 14:28:23 -0800

    feat: add socialURLs data, update header links refactor: delete unused fonts, clean up and compile scss

--- components/Layout/Header/Header.jsx
+++ components/Layout/Header/Header.jsx
@@ -1,42 +1,43 @@
-import styles from './Header.module.scss';
-import Link from 'next/link';
+import styles from "./Header.module.scss";
+import Link from "next/link";
 
-import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
-import { MdMenu, MdClose } from 'react-icons/md';
-import Switch from '../../Switch/Switch';
-import { useContext, useState } from 'react';
-import { ThemeContext } from '../../../store/ThemeContextProvider';
+import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+import { MdMenu, MdClose } from "react-icons/md";
+import Switch from "../../Switch/Switch";
+import { useContext, useState } from "react";
+import { ThemeContext } from "../../../store/ThemeContextProvider";
+import { socialURLs } from "../../../public/data";
 
 const Header = ({ projectsRef, skillsRef, aboutRef, contactRef }) => {
   const { theme } = useContext(ThemeContext);
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className={`${styles['header']} ${styles[theme]}`}>
-      <div className={styles['main-banner']}>
-        <Link className={styles['home-link']} href='/'>
+    <header className={`${styles["header"]} ${styles[theme]}`}>
+      <div className={styles["main-banner"]}>
+        <Link className={styles["home-link"]} href="/">
           <figure className={styles.logo}>
-            <span className={styles['home-icon']}>👹</span>
-            <span className={styles['main-title']}>laDev</span>
+            <span className={styles["home-icon"]}>👹</span>
+            <span className={styles["main-title"]}>laDev</span>
           </figure>
         </Link>
       </div>
 
-      <nav className={styles['nav']}>
+      <nav className={styles["nav"]}>
         <ul
-          className={`${styles['nav-links']} ${styles[theme]} ${
-            menuOpen ? styles.active : ''
+          className={`${styles["nav-links"]} ${styles[theme]} ${
+            menuOpen ? styles.active : ""
           }`}
         >
           <li
             onClick={() => {
               projectsRef.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
+                behavior: "smooth",
+                block: "start",
               });
               setMenuOpen(false);
             }}
-            className={`${styles['nav__link']}`}
+            className={`${styles["nav__link"]}`}
           >
             projects
           </li>
@@ -44,12 +45,12 @@ const Header = ({ projectsRef, skillsRef, aboutRef, contactRef }) => {
           <li
             onClick={() => {
               skillsRef.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'nearest',
+                behavior: "smooth",
+                block: "nearest",
               });
               setMenuOpen(false);
             }}
-            className={`${styles['nav__link']}`}
+            className={`${styles["nav__link"]}`}
           >
             skills
           </li>
@@ -57,65 +58,65 @@ const Header = ({ projectsRef, skillsRef, aboutRef, contactRef }) => {
           <li
             onClick={() => {
               aboutRef.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
+                behavior: "smooth",
+                block: "start",
               });
               setMenuOpen(false);
             }}
-            className={`${styles['nav__link']}`}
+            className={`${styles["nav__link"]}`}
           >
             me
           </li>
           <li
             onClick={() => {
               contactRef.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'end',
+                behavior: "smooth",
+                block: "end",
               });
               setMenuOpen(false);
             }}
-            className={`${styles['nav__link']}`}
+            className={`${styles["nav__link"]}`}
           >
             contact
           </li>
 
-          <li className={`${styles['nav__link']}`}>
+          <li className={`${styles["nav__link"]}`}>
             <a
-              title='Github'
-              className={styles['social-link']}
-              href='https://github.com/allanlalangan'
-              target='_blank'
-              rel='noreferrer noopener'
+              title="Github"
+              className={styles["social-link"]}
+              href={socialURLs.github}
+              target="_blank"
+              rel="noreferrer noopener"
             >
-              <FaGithub aria-hidden='true' className={styles['social-icon']} />
-              <span className={styles['social-link__text']}>Github</span>
+              <FaGithub aria-hidden="true" className={styles["social-icon"]} />
+              <span className={styles["social-link__text"]}>Github</span>
             </a>
           </li>
-          <li className={`${styles['nav__link']}`}>
+          <li className={`${styles["nav__link"]}`}>
             <a
-              title='LinkedIn'
-              className={styles['social-link']}
-              href='https://www.linkedin.com/in/allan-lalangan-16a86a243/'
-              target='_blank'
-              rel='noreferrer noopener'
+              title="LinkedIn"
+              className={styles["social-link"]}
+              href={socialURLs.linkedin}
+              target="_blank"
+              rel="noreferrer noopener"
             >
               <FaLinkedin
-                aria-hidden='true'
-                className={styles['social-icon']}
+                aria-hidden="true"
+                className={styles["social-icon"]}
               />
-              <span className={styles['social-link__text']}>LinkedIn</span>
+              <span className={styles["social-link__text"]}>LinkedIn</span>
             </a>
           </li>
-          <li className={`${styles['nav__link']}`}>
+          <li className={`${styles["nav__link"]}`}>
             <a
-              title='Twitter'
-              className={styles['social-link']}
-              href='https://twitter.com/allanladev'
-              target='_blank'
-              rel='noreferrer noopener'
+              title="Twitter"
+              className={styles["social-link"]}
+              href={socialURLs.twitter}
+              target="_blank"
+              rel="noreferrer noopener"
             >
-              <FaTwitter aria-hidden='true' className={styles['social-icon']} />
-              <span className={styles['social-link__text']}>Twitter</span>
+              <FaTwitter aria-hidden="true" className={styles["social-icon"]} />
+              <span className={styles["social-link__text"]}>Twitter</span>
             </a>
           </li>
           {/* <Link href='/'>
@@ -127,19 +128,19 @@ const Header = ({ projectsRef, skillsRef, aboutRef, contactRef }) => {
             </li>
           </Link> */}
         </ul>
-        <aside className={styles['menu-button__container']}>
+        <aside className={styles["menu-button__container"]}>
           <Switch />
           <button
-            aria-label='Theme Switch'
+            aria-label="Theme Switch"
             onClick={() => setMenuOpen(!menuOpen)}
-            className={`${styles['menu-button']} ${
-              menuOpen ? styles.active : ''
+            className={`${styles["menu-button"]} ${
+              menuOpen ? styles.active : ""
             }`}
           >
             {menuOpen ? (
-              <MdClose className={`${styles[theme]} ${styles['close-icon']}`} />
+              <MdClose className={`${styles[theme]} ${styles["close-icon"]}`} />
             ) : (
-              <MdMenu className={`${styles[theme]} ${styles['burger-icon']}`} />
+              <MdMenu className={`${styles[theme]} ${styles["burger-icon"]}`} />
             )}
           </button>
         </aside>
