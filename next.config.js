9e6b275f77b57da5373ad141332c3924c90c2cce
allanlalangan <allanlalangan@gmail.com>
2022-12-28 14:27:13 -0800

    feat: update hero button

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   reactStrictMode: true,
   swcMinify: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
