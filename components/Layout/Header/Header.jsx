a06316edaea857cb50d1cfb74f73a9e4b6b8f4f3
allanlalangan <allanlalangan@gmail.com>
2022-12-31 23:02:23 -0800

    feat: update scss mixins, compile sass

--- components/Layout/Header/Header.jsx
+++ components/Layout/Header/Header.jsx
@@ -9,117 +9,117 @@ import { ThemeContext } from "../../../store/ThemeContextProvider";
 import { socialURLs } from "../../../public/data";
 
 const Header = ({ projectsRef, skillsRef, aboutRef, contactRef }) => {
-  const { theme } = useContext(ThemeContext);
-  const [menuOpen, setMenuOpen] = useState(false);
+	const { theme } = useContext(ThemeContext);
+	const [menuOpen, setMenuOpen] = useState(false);
 
-  return (
-    <header className={`${styles["header"]} ${styles[theme]}`}>
-      <div className={styles["main-banner"]}>
-        <Link className={styles["home-link"]} href="/">
-          <figure className={styles.logo}>
-            <span className={styles["home-icon"]}>👹</span>
-            <span className={styles["main-title"]}>laDev</span>
-          </figure>
-        </Link>
-      </div>
+	return (
+		<header className={`${styles["header"]} ${styles[theme]}`}>
+			<div className={styles["main-banner"]}>
+				<Link className={styles["home-link"]} href="/">
+					<figure className={styles.logo}>
+						<span className={styles["home-icon"]}>👹</span>
+						<span className={styles["main-title"]}>laDev</span>
+					</figure>
+				</Link>
+			</div>
 
-      <nav className={styles["nav"]}>
-        <ul
-          className={`${styles["nav-links"]} ${styles[theme]} ${
-            menuOpen ? styles.active : ""
-          }`}
-        >
-          <li
-            onClick={() => {
-              projectsRef.current.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-              });
-              setMenuOpen(false);
-            }}
-            className={`${styles["nav__link"]}`}
-          >
-            projects
-          </li>
+			<nav className={styles["nav"]}>
+				<ul
+					className={`${styles["nav-links"]} ${styles[theme]} ${
+						menuOpen ? styles.active : ""
+					}`}
+				>
+					<li
+						onClick={() => {
+							projectsRef.current.scrollIntoView({
+								behavior: "smooth",
+								block: "start",
+							});
+							setMenuOpen(false);
+						}}
+						className={`${styles["nav__link"]}`}
+					>
+						projects
+					</li>
 
-          <li
-            onClick={() => {
-              skillsRef.current.scrollIntoView({
-                behavior: "smooth",
-                block: "nearest",
-              });
-              setMenuOpen(false);
-            }}
-            className={`${styles["nav__link"]}`}
-          >
-            skills
-          </li>
+					<li
+						onClick={() => {
+							skillsRef.current.scrollIntoView({
+								behavior: "smooth",
+								block: "nearest",
+							});
+							setMenuOpen(false);
+						}}
+						className={`${styles["nav__link"]}`}
+					>
+						skills
+					</li>
 
-          <li
-            onClick={() => {
-              aboutRef.current.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-              });
-              setMenuOpen(false);
-            }}
-            className={`${styles["nav__link"]}`}
-          >
-            me
-          </li>
-          <li
-            onClick={() => {
-              contactRef.current.scrollIntoView({
-                behavior: "smooth",
-                block: "end",
-              });
-              setMenuOpen(false);
-            }}
-            className={`${styles["nav__link"]}`}
-          >
-            contact
-          </li>
+					<li
+						onClick={() => {
+							aboutRef.current.scrollIntoView({
+								behavior: "smooth",
+								block: "start",
+							});
+							setMenuOpen(false);
+						}}
+						className={`${styles["nav__link"]}`}
+					>
+						me
+					</li>
+					<li
+						onClick={() => {
+							contactRef.current.scrollIntoView({
+								behavior: "smooth",
+								block: "end",
+							});
+							setMenuOpen(false);
+						}}
+						className={`${styles["nav__link"]}`}
+					>
+						contact
+					</li>
 
-          <li className={`${styles["nav__link"]}`}>
-            <a
-              title="Github"
-              className={styles["social-link"]}
-              href={socialURLs.github}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FaGithub aria-hidden="true" className={styles["social-icon"]} />
-              <span className={styles["social-link__text"]}>Github</span>
-            </a>
-          </li>
-          <li className={`${styles["nav__link"]}`}>
-            <a
-              title="LinkedIn"
-              className={styles["social-link"]}
-              href={socialURLs.linkedin}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FaLinkedin
-                aria-hidden="true"
-                className={styles["social-icon"]}
-              />
-              <span className={styles["social-link__text"]}>LinkedIn</span>
-            </a>
-          </li>
-          <li className={`${styles["nav__link"]}`}>
-            <a
-              title="Twitter"
-              className={styles["social-link"]}
-              href={socialURLs.twitter}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FaTwitter aria-hidden="true" className={styles["social-icon"]} />
-              <span className={styles["social-link__text"]}>Twitter</span>
-            </a>
-          </li>
-          {/* <Link href='/'>
+					<li className={`${styles["nav__link"]}`}>
+						<a
+							title="Github"
+							className={styles["social-link"]}
+							href={socialURLs.github}
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							<FaGithub aria-hidden="true" className={styles["social-icon"]} />
+							<span className={styles["social-link__text"]}>Github</span>
+						</a>
+					</li>
+					<li className={`${styles["nav__link"]}`}>
+						<a
+							title="LinkedIn"
+							className={styles["social-link"]}
+							href={socialURLs.linkedin}
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							<FaLinkedin
+								aria-hidden="true"
+								className={styles["social-icon"]}
+							/>
+							<span className={styles["social-link__text"]}>LinkedIn</span>
+						</a>
+					</li>
+					<li className={`${styles["nav__link"]}`}>
+						<a
+							title="Twitter"
+							className={styles["social-link"]}
+							href={socialURLs.twitter}
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							<FaTwitter aria-hidden="true" className={styles["social-icon"]} />
+							<span className={styles["social-link__text"]}>Twitter</span>
+						</a>
+					</li>
+					{/* <Link href='/'>
             <li
               className={`${styles['nav__link']} ${styles['cv-link']}`}
             >
@@ -127,25 +127,25 @@ const Header = ({ projectsRef, skillsRef, aboutRef, contactRef }) => {
               <FaFileDownload className={styles['cv-icon']} />
             </li>
           </Link> */}
-        </ul>
-        <aside className={styles["menu-button__container"]}>
-          <Switch />
-          <button
-            aria-label="Theme Switch"
-            onClick={() => setMenuOpen(!menuOpen)}
-            className={`${styles["menu-button"]} ${
-              menuOpen ? styles.active : ""
-            }`}
-          >
-            {menuOpen ? (
-              <MdClose className={`${styles[theme]} ${styles["close-icon"]}`} />
-            ) : (
-              <MdMenu className={`${styles[theme]} ${styles["burger-icon"]}`} />
-            )}
-          </button>
-        </aside>
-      </nav>
-    </header>
-  );
+				</ul>
+				<aside className={styles["menu-button__container"]}>
+					<Switch />
+					<button
+						aria-label="Theme Switch"
+						onClick={() => setMenuOpen(!menuOpen)}
+						className={`${styles["menu-button"]} ${
+							menuOpen ? styles.active : ""
+						}`}
+					>
+						{menuOpen ? (
+							<MdClose className={`${styles[theme]} ${styles["close-icon"]}`} />
+						) : (
+							<MdMenu className={`${styles[theme]} ${styles["burger-icon"]}`} />
+						)}
+					</button>
+				</aside>
+			</nav>
+		</header>
+	);
 };
 export default Header;
--- components/ProjectCard/ProjectCard.jsx
+++ components/ProjectCard/ProjectCard.jsx
@@ -1,101 +1,99 @@
-import Image from 'next/image';
-import { useContext } from 'react';
-import { ThemeContext } from '../../store/ThemeContextProvider';
-import { FaGithub, FaLink, FaSearch } from 'react-icons/fa';
-import { AiFillApi } from 'react-icons/ai';
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "../../store/ThemeContextProvider";
+import { FaGithub, FaLink, FaSearch } from "react-icons/fa";
+import { AiFillApi } from "react-icons/ai";
 
-import styles from './ProjectCard.module.scss';
-import TechIcon from '../ui/TechIcon/TechIcon';
+import styles from "./ProjectCard.module.scss";
+import TechIcon from "../ui/TechIcon/TechIcon";
 
 const ProjectCard = ({
-  project: { title, description, tech, image, repo, url, complete, demo },
+	project: { title, description, tech, image, repo, url, complete, demo },
 }) => {
-  const { theme } = useContext(ThemeContext);
-  return (
-    <article className={`${styles['container']} ${styles[theme]}`}>
-      <section className={styles['project-header']}>
-        <h2 className={styles.__title}>{title}</h2>
-        {!complete && (
-          <span className={styles['progress-message']}>
-            currently building...
-          </span>
-        )}
-      </section>
-      <section className={styles['tech-stack']}>
-        <h3 className={styles['tech-heading']}>Tech Stack</h3>
-        <ul className={`${styles['tech-list']} grid-row`}>
-          {tech?.map((t) => (
-            <li className={styles.tech} key={t}>
-              <TechIcon tech={t} styles={styles['tech-icon']} />
-              <span className={styles['tech-caption']}>{t}</span>
-            </li>
-          ))}
-        </ul>
-      </section>
+	const { theme } = useContext(ThemeContext);
+	return (
+		<article className={`${styles["container"]} ${styles[theme]}`}>
+			<section className={styles["project-header"]}>
+				<h2 className={styles.__title}>{title}</h2>
+				{!complete && (
+					<span className={styles["progress-message"]}>
+						currently building...
+					</span>
+				)}
+			</section>
+			<section className={styles["tech-stack"]}>
+				<h3 className={styles["tech-heading"]}>Tech Stack</h3>
+				<ul className={`${styles["tech-list"]} grid-row`}>
+					{tech?.map((t) => (
+						<li className={styles.tech} key={t}>
+							<TechIcon tech={t} styles={styles["tech-icon"]} />
+							<span className={styles["tech-caption"]}>{t}</span>
+						</li>
+					))}
+				</ul>
+			</section>
 
-      <section className={`${styles['previews']} `}>
-        <figure className={`${styles['__preview']}`}>
-          <Image className={styles.__image} src={image.src} alt={image.alt} />
-        </figure>
-        {/* <figure
+			<section className={`${styles["previews"]} `}>
+				<figure className={`${styles["__preview"]}`}>
+					<Image className={styles.__image} src={image.src} alt={image.alt} />
+				</figure>
+				{/* <figure
           className={`${styles['__preview']} ${styles['__preview--mobile']}`}
         ></figure> */}
-      </section>
-      <article className={`${styles['project-info']} `}>
-        <p>{description}</p>
-      </article>
-      {demo?.email && demo?.password && (
-        <section className={styles['demo']}>
-          <h4 className={`${styles['demo-heading']}`}>Try it out!</h4>
-          <article className={styles['demo-login-info']}>
-            <p className={styles['demo-field']}>
-              <span>
-                <strong>email: </strong>
-              </span>
-              <span className={styles['demo-email']}> {demo.email}</span>
-            </p>
-            <p className={styles['demo-field']}>
-              <span>
-                <strong>password: </strong>
-              </span>
-              <span className={styles['demo-password']}> {demo.password}</span>
-            </p>
-          </article>
-        </section>
-      )}
-      <section className={`${styles.links}`}>
-        <div className={styles['button-group']}>
-          <a
-            className={styles.link}
-            href={url}
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            <button
-              disabled={!url}
-              className={`${styles['project-link-button']} ${styles['project-link-button--live']} ${styles[theme]}`}
-            >
-              <FaLink className={styles['project-link-button__icon']} />
-              <span className={styles['project-link-button__text']}>Demo</span>
-            </button>
-          </a>
-          <div className={styles['button-divider']} />
-          <a
-            className={styles.link}
-            href={repo}
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            <button
-              className={`${styles['project-link-button']} ${styles['project-link-button--github']} ${styles[theme]}`}
-            >
-              <FaGithub className={styles['project-link-button__icon']} />
-              <span className={styles['project-link-button__text']}>Code</span>
-            </button>
-          </a>
-        </div>
-      </section>
-    </article>
-  );
+			</section>
+			<article className={`${styles["project-info"]} `}>{description}</article>
+			{demo?.email && demo?.password && (
+				<section className={styles["demo"]}>
+					<h4 className={`${styles["demo-heading"]}`}>Try it out!</h4>
+					<article className={styles["demo-login-info"]}>
+						<p className={styles["demo-field"]}>
+							<span>
+								<strong>email: </strong>
+							</span>
+							<span className={styles["demo-email"]}> {demo.email}</span>
+						</p>
+						<p className={styles["demo-field"]}>
+							<span>
+								<strong>password: </strong>
+							</span>
+							<span className={styles["demo-password"]}> {demo.password}</span>
+						</p>
+					</article>
+				</section>
+			)}
+			<section className={`${styles.links}`}>
+				<div className={styles["button-group"]}>
+					<a
+						className={styles.link}
+						href={url}
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<button
+							disabled={!url}
+							className={`${styles["project-link-button"]} ${styles["project-link-button--live"]} ${styles[theme]}`}
+						>
+							<FaLink className={styles["project-link-button__icon"]} />
+							<span className={styles["project-link-button__text"]}>Demo</span>
+						</button>
+					</a>
+					<div className={styles["button-divider"]} />
+					<a
+						className={styles.link}
+						href={repo}
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<button
+							className={`${styles["project-link-button"]} ${styles["project-link-button--github"]} ${styles[theme]}`}
+						>
+							<FaGithub className={styles["project-link-button__icon"]} />
+							<span className={styles["project-link-button__text"]}>Code</span>
+						</button>
+					</a>
+				</div>
+			</section>
+		</article>
+	);
 };
 export default ProjectCard;
